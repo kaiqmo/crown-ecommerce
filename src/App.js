c69e0cf43821691d2f850021dfc281cd6497092e
kaiqmo <kaiqmo@gmail.com>
2020-01-24 23:06:16 -0300

    header component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Homepage from './pages/homepage.component';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 
+import Header from './components/header-component/header-component.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
